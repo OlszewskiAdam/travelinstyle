1693292330e3e05601dbbb0ec0549ab3ffeceae5
Adam Olszewski <adam_olszewski@poczta.onet.pl>
2017-12-05 22:08:08 +0100

    tablet02

--- js/app.js
+++ js/app.js
@@ -1,4 +1,34 @@
+let myWindow = $(window);
+
 let burgerMenu = $('#nav-icon');
-burgerMenu.on("click", (event)=>{
+burgerMenu.on("click", function(event) {
+
     burgerMenu.toggleClass('open');
 })
+
+let footerItems = $(".footer_menu_item");
+
+footerItems.on('click', function(event){
+    let windowWidth = $(window).width();
+    if(windowWidth < 900){
+        let list = $(this).find("ul");
+        let icon = $(this).find("h3").find("i");
+        if(list.css("display") == "block"){
+            list.slideUp(400);
+            icon.removeClass("inverted");
+        }
+        else{
+            list.slideDown(400);
+            icon.addClass("inverted");
+        }
+    }
+});
+myWindow.on("resize", function(event){
+    let windowWidth = myWindow.width();
+    let lists = footerItems.find("ul");
+    if(windowWidth >= 900){
+        lists.css("display", "block");
+    }
+    
+});
+console.log(footerItems);
